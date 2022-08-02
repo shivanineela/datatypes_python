fcfaf853f08b3182ad7b11d5b210a1565594acef
shivanineela <109790213+shivanineela@users.noreply.github.com>
2022-08-02 18:35:49 +0530

    Add files via upload

---
+++ datatypes_python.py
@@ -0,0 +1,27 @@
+# print(type(65))
+# print(type(00))
+# print(type(245.89))
+# print(type(.890))
+# print(type('hi'))
+# print(type("hi"))
+# print(type([1,5,9,0]))
+# print(type((1,5,9,0)))
+# print(type((7,)))
+
+# a=9
+# print(type(a))
+# b=56,
+# print(type(b))
+# c=1,7,0
+# print(type(c))
+
+# g=[987]
+# print(type(g))
+# h=[6,7,8,9]
+# print(type(h))
+
+# a=1,'a'
+# print(type(a))
+
+# t=None
+# print(type(t))
